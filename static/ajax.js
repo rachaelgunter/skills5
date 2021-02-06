4ed1d0076f4e54d4f1c65a5543b855898fceb5d4
vagrant <rachaelegunter@gmail.com>
2021-02-06 03:34:04 -0500

    safety

---
+++ static/ajax.js
@@ -0,0 +1,25 @@
+const findHumanId = () => {
+   
+      <tr>
+        <td>{itemName}</td>
+      </tr>
+    ;
+  };
+  
+const formInputs = {
+    'fname': $('#type-field').val(),
+    'lname': $('#type-field').val()
+    'email': $('#type-field').val()
+  };
+
+
+button.addEventListener('click' (evt) => {
+
+  name(some kind of function);
+});
+
+$.get('/api/human/<human_id>', formData, (res) => {
+    
+    alert(res);
+  });
+});
